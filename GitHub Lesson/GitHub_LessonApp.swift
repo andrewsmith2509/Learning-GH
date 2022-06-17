3840cdcdda84bc82a3ea4463b7367450c7429de0
Andrew <andrewsmith2509@gmail.com>
2022-06-17 16:10:00 -0700

    Initial Commit

---
+++ GitHub Lesson/GitHub_LessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_LessonApp.swift
+//  GitHub Lesson
+//
+//  Created by Andrew Smith on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_LessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
